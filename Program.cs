bc56fb3b173bd16a8a43787a4f04475dc7f81e71
Sun Zhao <zixiaojindao@gmail.com>
2013-10-07 17:14:09 +0800

    init

---
+++ Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Diagnostics;
+using System.Runtime.InteropServices;
+
+namespace testsharp
+{
+
+    class Program
+    {
+        [DllImport("User32.dll")]
+        static extern bool SetForegroundWindow(IntPtr hWnd);
+        /// <summary>
+        /// filter function
+        /// </summary>
+        /// <param name="hWnd"></param>
+        /// <param name="lParam"></param>
+        /// <returns></returns>
+        public delegate bool EnumDelegate(IntPtr hWnd, int lParam);
+
+        /// <summary>
+        /// check if windows visible
+        /// </summary>
+        /// <param name="hWnd"></param>
+        /// <returns></returns>
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool IsWindowVisible(IntPtr hWnd);
+
+        /// <summary>
+        /// return windows text
+        /// </summary>
+        /// <param name="hWnd"></param>
+        /// <param name="lpWindowText"></param>
+        /// <param name="nMaxCount"></param>
+        /// <returns></returns>
+        [DllImport("user32.dll", EntryPoint = "GetWindowText",
+        ExactSpelling = false, CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern int GetWindowText(IntPtr hWnd, StringBuilder lpWindowText, int nMaxCount);
+
+        /// <summary>
+        /// enumarator on all desktop windows
+        /// </summary>
+        /// <param name="hDesktop"></param>
+        /// <param name="lpEnumCallbackFunction"></param>
+        /// <param name="lParam"></param>
+        /// <returns></returns>
+        [DllImport("user32.dll", EntryPoint = "EnumDesktopWindows",
+        ExactSpelling = false, CharSet = CharSet.Auto, SetLastError = true)]
+        public static extern bool EnumDesktopWindows(IntPtr hDesktop, EnumDelegate lpEnumCallbackFunction, IntPtr lParam);
+
+        /// <summary>
+        /// entry point of the program
+        /// </summary>
+        static void Main()
+        {
+            var collection = new List<KeyValuePair<string, IntPtr>>();
+            EnumDelegate filter = delegate(IntPtr hWnd, int lParam)
+            {
+                StringBuilder strbTitle = new StringBuilder(255);
+                int nLength = GetWindowText(hWnd, strbTitle, strbTitle.Capacity + 1);
+                string strTitle = strbTitle.ToString();
+
+                if (IsWindowVisible(hWnd) && string.IsNullOrEmpty(strTitle) == false)
+                {
+                    collection.Add(new KeyValuePair<string, IntPtr>(strTitle, hWnd));
+                }
+                return true;
+            };
+
+            if (EnumDesktopWindows(IntPtr.Zero, filter, IntPtr.Zero))
+            {
+                foreach (var item in collection)
+                {
+                    Console.WriteLine(item.Key + " " + item.Value);
+                    string line = Console.ReadLine();
+                    if(line.Length != 0)
+                    {
+                        SetForegroundWindow(item.Value);
+                    }
+                }
+            }
+            Console.Read();
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,1 @@
+###TypeAnyWhere as you want!
